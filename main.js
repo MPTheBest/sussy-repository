55cb974ce982545b73a532279fa35266c4e5b6a4
MissionaryPoseTheBest <m1ss1onaryposethebestxxxx@gmail.com>
2023-02-23 20:14:10 +0300

    init (egg counter)

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("SIUUUU");
   res.end();
-}).listen(3030);
+}).listen(8080);
 
 var num = 1;
 console.log(`variable: ${num}`);
 console.log(`the server started; https://localhost:3030`);
+
+const fs = require('fs');
+
+//egg counter
+let result = fs.readFileSync('txt.txt', 'utf-8');
+console.log(result);
+fs.writeFileSync('txt.txt', result + 'egg\n');
